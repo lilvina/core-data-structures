250979d65d312dd65806992ec17be91dff9dbc7b
lilvina <davinaleo2007@yahoo.com>
2017-01-25 09:36:32 -0800

    made some changes for priority queue

--- spec/pQueue.js
+++ spec/pQueue.js
@@ -1,84 +1,58 @@
 import chai, { expect } from 'chai'
 import chaiChange from 'chai-change'
-import pQueue from '../src/pQueue'
+import PriorityQueue from '../src/pQueue'
 
 chai.use(chaiChange)
 
-describe('pQueue', () => {
+describe('PriorityQueue', () => {
   'use strict'
 
   it('exists', () => {
-    expect(pQueue).to.be.a('function')
+    expect(PriorityQueue).to.be.a('function')
   })
 
   context('enqueue()', () => {
     it('adds an element with priority to the back of the queue.', () => {
-      const aQueue = new pQueue()
+      const queue = new PriorityQueue()
 
-      aQueue.enqueue('dude', 90)
-      aQueue.enqueue('cool', 20)
-      aQueue.enqueue('turd', 50)
-      expect(aQueue.enqueue('foo',70)).to.eql([{name: 'dude', priority: 90}, {name: 'cool',  priority: 20}, {name: 'turd', priority: 50}, {name: 'foo', priority: 70}])
+      queue.enqueue('dude', 90)
+      queue.enqueue('cool', 20)
+      queue.enqueue('turd', 50)
+
+      expect(queue.enqueue('foo',70)).to.eql([
+        {name: 'dude', priority: 90}, 
+        {name: 'cool',  priority: 20}, 
+        {name: 'turd', priority: 50}, 
+        {name: 'foo', priority: 70}
+      ])
     })
   })
 
-  // context('front()', () => {
-  //   it('returns the front element (highest priority) in the queue.', () => {
-  //     const aQueue = new pQueue()
-
-  //     aQueue.enqueue('baz', 300)
-  //     aQueue.enqueue('bar', 200)
-
-  //     expect(() => aQueue.front().to.eql([{name: 'baz', priority: 300}]))
-  //   })
-
-  //   it('returns null if the queue is empty.', () => {
-  //     const aQueue = new pQueue()
-
-  //     expect(() => aQueue.front().to.eql(null))
-  //   })
-  // })
-
-  // context('back()', () => {
-  //   it('returns the back element (lowest priority) in the queue.', () {
-  //     const aQueue = new pQueue()
-
-  //     aQueue.enqueue('bar', 200)
-  //     aQueue.enqueue('foo', 100)
-
-  //     expect(() => aQueue.back().to.eql([{name: 'foo', priority: 100}]))
-  //   })
-
-  //   it('returns null if the queue is empty.', () => {
-  //     const aQueue = new pQueue()
-
-  //     expect(() => pQueue.back().to.eql(null))
-  //   })
-  // })
-
   context('isEmpty()', () => {
     it('returns true if the queue is empty.', () => {
-      const aQueue = new pQueue()
+      const queue = new PriorityQueue()
 
-      expect(() => aQueue.isEmpty().to.eql(true))
+      expect(queue.isEmpty()).to.be.true
     })
 
-    it('returns false if queue is false.', () => {
-      const aQueue = new pQueue()
-      aQueue.enqueue('dude', 90)
+    it('returns false if queue is empty.', () => {
+      const queue = new PriorityQueue()
 
-      expect(() => aQueue.isEmpty().to.eql(false))
+      queue.enqueue('dude', 90)
+
+      expect(() => queue.isEmpty().to.eql(false))
     })
   })
 
   context('length()', () => {
     it('returns the number of elements in the queue.', () => {
-      const aQueue = new pQueue()
-      aQueue.enqueue('dude', 90)
-      aQueue.enqueue('cool', 20)
-      aQueue.enqueue('turd', 50)
+      const queue = new PriorityQueue()
 
-      expect(aQueue.length()).to.eql(3)
+      queue.enqueue('dude', 90)
+      queue.enqueue('cool', 20)
+      queue.enqueue('turd', 50)
+
+      expect(queue.length()).to.eql(3)
     })
   })
 })
--- src/pQueue.js
+++ src/pQueue.js
@@ -3,19 +3,19 @@
 export default class pQueue {
   constructor() {
     this.storage = []
-    this.top = 0
+    this.front = 0
   }
 
   enqueue(name, priority) {
-    this.storage[this.top++] = {name, priority}
+    this.storage[this.front++] = {name, priority}
     return this.storage
   }
 
   isEmpty() {
-    return this.top === 0
+    return this.front === 0
   }
 
   length() {
-    return this.top
+    return this.front
   }
 }
